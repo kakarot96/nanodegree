2eaf5c1fe8681335b7b85fc539161d0364ed926d
kunal1996 <kunalaggarwal58@gmail.com>
2016-08-23 23:24:17 +0530

    custom adapter completed

---
+++ PopularMovies22/app/src/main/java/com/example/kakarot/popularmovies2/ImageListAdapter.java
@@ -0,0 +1,46 @@
+package com.example.kakarot.popularmovies2;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
+
+/**
+ * Created by kakarot on 8/23/2016.
+ */
+public class ImageListAdapter extends ArrayAdapter {
+
+    private Context context;
+    private LayoutInflater inflater;
+
+    private String[] imageUrls;
+
+    public ImageListAdapter(Context context, String[] imageUrls) {
+        super(context, R.layout.grid_item_movie, imageUrls);
+
+        this.context = context;
+        this.imageUrls = imageUrls;
+
+        inflater = LayoutInflater.from(context);
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        if (null == convertView) {
+            convertView = inflater.inflate(R.layout.grid_item_movie, parent, false);
+        }
+
+        Picasso
+                .with(context)
+                .load(imageUrls[position])
+                .fit() // will explain later
+                .into((ImageView) convertView);
+
+        return convertView;
+    }
+
+}
---
+++ PopularMovies22/app/src/main/java/com/example/kakarot/popularmovies2/MoviesDB.java
@@ -0,0 +1,274 @@
+package com.example.kakarot.popularmovies2;
+
+
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.GridView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class MoviesDB extends Fragment {
+    private ImageListAdapter mForecastAdapter;
+
+    public MoviesDB() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+        inflater.inflate(R.menu.moviesfragment,menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        int id = item.getItemId();
+        if (id == R.id.action_refresh) {
+            updateWeather();
+
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void updateWeather() {
+        FetchMoviesTask moviesTask=new FetchMoviesTask();
+        moviesTask.execute("");
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View rootView=inflater.inflate(R.layout.fragment_movies_db, container, false);
+
+        String[] data={
+                "http://i.imgur.com/rFLNqWI.jpg",
+                "http://i.imgur.com/C9pBVt7.jpg",
+                "http://i.imgur.com/rT5vXE1.jpg",
+                "http://i.imgur.com/aIy5R2k.jpg",
+                "http://i.imgur.com/MoJs9pT.jpg",
+                "http://i.imgur.com/S963yEM.jpg",
+                "http://i.imgur.com/rLR2cyc.jpg",
+                "http://i.imgur.com/SEPdUIx.jpg",
+                "http://i.imgur.com/aC9OjaM.jpg",
+                "http://i.imgur.com/76Jfv9b.jpg",
+                "http://i.imgur.com/fUX7EIB.jpg",
+                "http://i.imgur.com/syELajx.jpg",
+                "http://i.imgur.com/COzBnru.jpg",
+                "http://i.imgur.com/Z3QjilA.jpg",
+        };
+      // List<String> weekForecast = new ArrayList<String>(Arrays.asList(data));
+
+        mForecastAdapter =
+                new ImageListAdapter(getActivity(),data);
+        GridView gridView = (GridView) rootView.findViewById(R.id.gridview);
+       gridView.setAdapter(mForecastAdapter);
+
+
+
+
+
+
+        return rootView;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+    public class FetchMoviesTask extends AsyncTask<String, Void, Movie[]> {
+
+        private final String LOG_TAG = FetchMoviesTask.class.getSimpleName();
+
+
+
+
+
+        /**
+         * Take the String representing the complete forecast in JSON Format and
+         * pull out the data we need to construct the Strings needed for the wireframes.
+         * <p/>
+         * Fortunately parsing is easy:  constructor takes the JSON string and converts it
+         * into an Object hierarchy for us.
+         */
+        private Movie[] getMoviesDataFromJson(String moviesJsonStr)
+                throws JSONException {
+
+            // These are the names of the JSON objects that need to be extracted.
+            final String OWM_LIST = "results";
+            final String OWM_DESCRIPTION = "poster_path";
+
+           // JSONObject moviesJson = new JSONObject(moviesJsonStr);
+          //  JSONArray moviesArray = moviesJson.getJSONArray(OWM_LIST);
+
+
+            JSONObject mainObject = new JSONObject(moviesJsonStr);
+            JSONArray resultsArray = mainObject.getJSONArray("results");
+            Movie[] resultStrs = new Movie[resultsArray.length()];
+            for (int i = 0; i < resultsArray.length(); i++) {
+                // For now, using the format "Day, description, hi/low"
+                JSONObject indexObject = resultsArray.getJSONObject(i);
+                Movie indexMovie = new Movie();
+                indexMovie.setBackdrop_path(indexObject.getString("backdrop_path"));
+                indexMovie.setId(indexObject.getInt("id"));
+                indexMovie.setOriginal_title(indexObject.getString("original_title"));
+                indexMovie.setOverview(indexObject.getString("overview"));
+                indexMovie.setRelease_date(indexObject.getString("release_date"));
+                indexMovie.setPoster_path(indexObject.getString("poster_path"));
+                indexMovie.setPopularity(indexObject.getDouble("popularity"));
+                indexMovie.setTitle(indexObject.getString("title"));
+                indexMovie.setVote_average(indexObject.getInt("vote_average"));
+                indexMovie.setVote_count(indexObject.getInt("vote_count"));
+
+                resultStrs[i]=indexMovie; // Add each item to the list
+
+            }
+
+            for (Movie s : resultStrs) {
+                Log.v(LOG_TAG, "Forecast entry: " + s);
+            }
+            return resultStrs;
+
+        }
+
+        @Override
+        protected Movie[] doInBackground(String... params) {
+            // These two need to be declared outside the try/catch
+            // so that they can be closed in the finally block.
+            if (params.length == 0) {
+                return null;
+            }
+            HttpURLConnection urlConnection = null;
+            BufferedReader reader = null;
+
+            // Will contain the raw JSON response as a string.
+            String moviesJsonStr = null;
+
+            String format = "json";
+
+
+            try {
+                // Construct the URL for the OpenWeatherMap query
+                // Possible parameters are avaiable at OWM's forecast API page, at
+                // http://openweathermap.org/API#forecast
+                final String MOVIES_BASE_URL =
+                        "http://api.themoviedb.org/3/movie/popular?";
+
+                final String APPID_PARAM = "api_key";
+
+                Uri builtUri = Uri.parse(MOVIES_BASE_URL).buildUpon()
+                        .appendQueryParameter(APPID_PARAM, "8bbd2f5a1f269e6fdbec51aabd92b747")
+                        .build();
+
+                URL url = new URL(builtUri.toString());
+
+                Log.v(LOG_TAG, "Built URI " + builtUri.toString());
+
+                // Create the request to OpenWeatherMap, and open the connection
+                urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.setRequestMethod("GET");
+                urlConnection.connect();
+
+                // Read the input stream into a String
+                InputStream inputStream = urlConnection.getInputStream();
+                StringBuffer buffer = new StringBuffer();
+                if (inputStream == null) {
+                    // Nothing to do.
+                    return null;
+                }
+                reader = new BufferedReader(new InputStreamReader(inputStream));
+
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    // Since it's JSON, adding a newline isn't necessary (it won't affect parsing)
+                    // But it does make debugging a *lot* easier if you print out the completed
+                    // buffer for debugging.
+                    buffer.append(line + "\n");
+                }
+
+                if (buffer.length() == 0) {
+                    // Stream was empty.  No point in parsing.
+                    return null;
+                }
+                moviesJsonStr = buffer.toString();
+            } catch (IOException e) {
+                Log.e(LOG_TAG, "Error ", e);
+                // If the code didn't successfully get the weather data, there's no point in attemping
+                // to parse it.
+                return null;
+            } finally {
+                if (urlConnection != null) {
+                    urlConnection.disconnect();
+                }
+                if (reader != null) {
+                    try {
+                        reader.close();
+                    } catch (final IOException e) {
+                        Log.e(LOG_TAG, "Error closing stream", e);
+                    }
+                }
+            }
+
+            try {
+                return getMoviesDataFromJson(moviesJsonStr);
+            } catch (JSONException e) {
+                Log.e(LOG_TAG, e.getMessage(), e);
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Movie[] result) {
+            super.onPostExecute(result);
+            //  String[] result = new String[0];
+            if (result != null) {
+                mForecastAdapter.clear();
+                for (Movie dayForecastStr : result) {
+                    mForecastAdapter.add(dayForecastStr);
+                }
+            }
+        }
+    }
+
+
+}
